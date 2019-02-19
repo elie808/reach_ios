36061ddc99686ec07daed642c6e6899b610fd5ba
elie808 <elie808.k@gmail.com>
2019-02-20 00:43:55 +0200

    Add constants file

---
+++ reach-ios/Constants/Constants.swift
@@ -0,0 +1,15 @@
+//
+//  Constants.swift
+//  reach-ios
+//
+//  Created by Elie El Khoury on 2/20/19.
+//  Copyright © 2019 Elie El Khoury. All rights reserved.
+//
+
+import UIKit
+
+struct Constants {
+    static let reachEmail = "reach@channelpro.co"
+    static let pointsEmail = "points@channelpro.co"
+}
+
---
+++ reach-ios/ViewControllers/PerformanceViewController/PerformanceViewController.swift
@@ -0,0 +1,30 @@
+//
+//  PerformanceViewController.swift
+//  reach-ios
+//
+//  Created by Elie El Khoury on 2/19/19.
+//  Copyright © 2019 Elie El Khoury. All rights reserved.
+//
+
+import UIKit
+
+class PerformanceViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
