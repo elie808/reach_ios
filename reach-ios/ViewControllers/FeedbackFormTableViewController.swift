8409214dadb603b8c18178e10bea739a89ff0ce2
elie808 <elie808.k@gmail.com>
2019-02-21 03:15:51 +0200

    Add datasource and finishing touches to UI on FeedbackVC

--- reach-ios/ViewControllers/FeedbackFormTableViewController.swift
+++ reach-ios/ViewControllers/FeedbackFormTableViewController.swift
@@ -8,30 +8,87 @@
 
 import UIKit
 
+struct FeedbackRating {
+    var isSelected : Bool = false
+    let value : Int
+}
+
+class FeedbackFormCell: UITableViewCell {
+    @IBOutlet weak var cellAccessoryImageView : UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+    
+    public func configure(with feedback:FeedbackRating) {
+        if feedback.isSelected == true {
+            cellAccessoryImageView.backgroundColor = .white
+        } else {
+            cellAccessoryImageView.backgroundColor = .clear
+        }
+    }
+}
+
 class FeedbackFormTableViewController: UITableViewController {
 
     // MARK: - Properties
     
+    var ratingList : [FeedbackRating] = []
+    
     // MARK: - Outlets
     
+    @IBOutlet var toolBar: UIToolbar!
     @IBOutlet weak var textView: UITextView!
     
     // MARK: - Views Life Cycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        textView.inputAccessoryView = toolBar
+        
+        ratingList = [FeedbackRating(isSelected: false, value: 1), FeedbackRating(isSelected: false, value: 2), FeedbackRating(isSelected: false, value: 3), FeedbackRating(isSelected: false, value: 4), FeedbackRating(isSelected: false, value: 5)]
     }
 
-//    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-//        
-//        switch indexPath.row {
-//            
-//        case 0:
-//            return UITableViewCell()
-//        default:
-//            return UITableViewCell()
-//        }
-//    }
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 7
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        switch indexPath.row {
+        case 1...5:
+            let cell = super.tableView(tableView, cellForRowAt: indexPath) as! FeedbackFormCell
+            cell.configure(with: ratingList[indexPath.row-1])
+            return cell
+            
+        default: return super.tableView(tableView, cellForRowAt: indexPath)
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        switch indexPath.row {
+        case 1...5:
+            for _ in 0...ratingList.count {
+                ratingList[indexPath.row-1].isSelected = false
+            }
+            ratingList[indexPath.row-1].isSelected = true
+            
+            tableView.reloadData()
+            
+        default: return
+        }
+    }
+    
+    // MARK: - Actions
+    
+    @IBAction func didTapDone(_ sender: UIBarButtonItem) {
+        textView.resignFirstResponder()
+    }
+    
+    @IBAction func didTapSubmit(_ sender: UIButton) {
+        
+    }
     
     /*
     // MARK: - Navigation
@@ -42,5 +99,12 @@ class FeedbackFormTableViewController: UITableViewController {
         // Pass the selected object to the new view controller.
     }
     */
+}
 
+extension FeedbackFormTableViewController : UITextViewDelegate {
+    
+    func textViewDidBeginEditing(_ textView: UITextView) {
+        print("EHGGG  r")
+        
+    }
 }
