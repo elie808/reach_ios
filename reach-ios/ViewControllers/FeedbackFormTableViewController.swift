9b91927cb41b77642e296cc5e73a443bfc22f579
elie808 <elie808.k@gmail.com>
2019-02-21 02:30:18 +0200

    Create UI for Feedback form

---
+++ reach-ios/ViewControllers/FeedbackFormTableViewController.swift
@@ -0,0 +1,46 @@
+//
+//  FeedbackFormTableViewController.swift
+//  reach-ios
+//
+//  Created by Elie El Khoury on 2/21/19.
+//  Copyright © 2019 Elie El Khoury. All rights reserved.
+//
+
+import UIKit
+
+class FeedbackFormTableViewController: UITableViewController {
+
+    // MARK: - Properties
+    
+    // MARK: - Outlets
+    
+    @IBOutlet weak var textView: UITextView!
+    
+    // MARK: - Views Life Cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+//    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        
+//        switch indexPath.row {
+//            
+//        case 0:
+//            return UITableViewCell()
+//        default:
+//            return UITableViewCell()
+//        }
+//    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
