1b9944e2ad594e359e8cc37f5d27b231573af6d4
elie808 <elie808.k@gmail.com>
2019-03-28 16:23:37 +0200

    Create DatePickerView UI and class

---
+++ reach-ios/Views/DatePickerView.swift
@@ -0,0 +1,59 @@
+//
+//  DatePickerView.swift
+//  reach-ios
+//
+//  Created by Elie El Khoury on 3/28/19.
+//  Copyright © 2019 Elie El Khoury. All rights reserved.
+//
+
+import UIKit
+
+protocol KeyboardDelegate: class {
+    func keyWasTapped(character: String)
+    func backspaceTapped()
+    func searchTapped()
+}
+
+class DatePickerView: UIView {
+    
+    // MARK: - Outlets
+    
+    @IBOutlet weak var datePicker : UIDatePicker!
+    
+    // MARK: - Properties
+    
+    weak var delegate: KeyboardDelegate?
+    
+    // MARK: - Initialization
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        initializeSubviews()
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        initializeSubviews()
+    }
+    
+    func initializeSubviews() {
+        let xibFileName = "DatePickerView" // xib extention not included
+        let view = Bundle.main.loadNibNamed(xibFileName, owner: self, options: nil)?[0] as! UIView
+        self.addSubview(view)
+        view.frame = self.bounds
+    }
+    
+    // MARK:- Actions
+    
+    @IBAction func didTapDone(_ sender: UIBarButtonItem) {
+        if delegate != nil {
+            //            self.delegate?.keyWasTapped(character: sender.titleLabel!.text!) // could alternatively send a tag value
+        }
+    }
+    
+    //    @IBAction func keyTapped(sender: UIButton) {
+    //        if delegate != nil {
+    //            self.delegate?.keyWasTapped(character: sender.titleLabel!.text!) // could alternatively send a tag value
+    //        }
+    //    }
+}
