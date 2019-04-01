11c9a4ab5685cbc2dc9661f60cb2193f5b3e3bac
elie808 <elie808.k@gmail.com>
2019-04-01 17:06:32 +0300

    Re-eable push notification code. Change the ressources URL to new address

--- reach-ios/AppDelegate+Notifications.swift
+++ reach-ios/AppDelegate+Notifications.swift
@@ -13,62 +13,53 @@ import OneSignal
 let oneSignalAppID = "4b5535c7-4e14-4ae1-89aa-97577bc469df"
 let oneSignalAPIKey = "ZjYxY2UyMDktMTg2ZS00MTQyLWIwMTItNGIwMDViMDNjYjU1"
 
-let zubara = "f396444c-227a-4aa8-b8e9-e16ade1556b6"
-
 extension AppDelegate {
     
     func configureOneSignal(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) {
-    
-        let notificationReceivedBlock: OSHandleNotificationReceivedBlock = { notification in
-            
-            print("Received Notification: \(notification!.payload.notificationID)")
-        }
-        
-        let notificationOpenedBlock: OSHandleNotificationActionBlock = { result in
-            // This block gets called when the user reacts to a notification received
-            let payload: OSNotificationPayload = result!.notification.payload
-            
-            var fullMessage = payload.body
-            print("Message = \(fullMessage)")
-            
-            if payload.additionalData != nil {
-                if payload.title != nil {
-                    let messageTitle = payload.title
-                    print("Message Title = \(messageTitle!)")
-                }
-                
-                let additionalData = payload.additionalData
-                if additionalData?["actionSelected"] != nil {
-                    fullMessage = fullMessage! + "\nPressed ButtonID: \(additionalData!["actionSelected"])"
-                }
-            }
-        }
-        
-        
-        
         
         let onesignalInitSettings = [kOSSettingsKeyAutoPrompt: true]
         
         OneSignal.initWithLaunchOptions(launchOptions, appId: oneSignalAppID, handleNotificationAction: nil, settings: onesignalInitSettings)
         
+        print("Push notificatio STATE: ")
+        let status: OSPermissionSubscriptionState = OneSignal.getPermissionSubscriptionState()
+        
+        let hasPrompted = status.permissionStatus.hasPrompted
+        print("hasPrompted = \(hasPrompted)")
+        let userStatus = status.permissionStatus.status
+        print("userStatus = \(userStatus)")
+        
+        let isSubscribed = status.subscriptionStatus.subscribed
+        print("isSubscribed = \(isSubscribed)")
+        let userSubscriptionSetting = status.subscriptionStatus.userSubscriptionSetting
+        print("userSubscriptionSetting = \(userSubscriptionSetting)")
+        let userID = status.subscriptionStatus.userId
+        print("userID = \(userID ?? "")")
+        let pushToken = status.subscriptionStatus.pushToken
+        print("pushToken = \(String(describing: pushToken))")
+
         OneSignal.inFocusDisplayType = OSNotificationDisplayType.notification
         
         /////////////
-        let pushObj = PushNotification(id : oneSignalAppID, onesignal_id: nil)
-        let oneSignal = Resource<PushNotification>(url: URL(string: NetworkingConstants.oneSignalID)!, method: HttpMethod<PushNotification>.post(pushObj))
         
         // Recommend moving the below line to prompt for push after informing the user about how your app will use them.
         OneSignal.promptForPushNotifications(userResponse: { accepted in
             print("\n \n \n")
             print("User accepted notifications: \(accepted)")
             
-            URLSession.shared.load(oneSignal) { (response, status) in
-                print("ONE SIGNAL RESPOSNE:")
-                print(response)
-                print("\n \n \n")
+            if let token = pushToken {
+            
+                let pushObj = PushNotification(id : token, onesignal_id: nil)
+                let oneSignal = Resource<PushNotification>(url: URL(string: NetworkingConstants.oneSignalID)!, method: HttpMethod<PushNotification>.post(pushObj))
+                
+                URLSession.shared.load(oneSignal) { (response, status) in
+                    print("\n \n \nONE SIGNAL RESPOSNE:")
+                    print(response!)
+                    print("\n \n \n")
+                }
             }
+            
         })
-
     }
 
     func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable: Any]) {
